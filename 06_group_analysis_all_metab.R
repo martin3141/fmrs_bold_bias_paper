820c04669d2065eb0c7382330e6387bddf67ecf9
Martin Wilson <martin@pipegrep.co.uk>
2026-02-18 10:03:11 +0000

    plotting update

--- 06_group_analysis_all_metab.R
+++ 06_group_analysis_all_metab.R
@@ -117,6 +117,11 @@ for (n in 1:length(run_ids)) {
   Glu_perc <- all_res[all_res$run_id == id,]$Glu / av_rest * 100 - 100
   all_res[all_res$run_id == id,]$Glu_perc <- Glu_perc
   
+  # Asc
+  av_rest <- mean(all_res[all_res$run_id == id & all_res$state == "REST",]$Asc)
+  Asc_perc <- all_res[all_res$run_id == id,]$Asc / av_rest * 100 - 100
+  all_res[all_res$run_id == id,]$Asc_perc <- Asc_perc
+  
   # Lac
   av_rest <- mean(all_res[all_res$run_id == id & all_res$state == "REST",]$Lac)
   Lac_perc <- all_res[all_res$run_id == id,]$Lac / av_rest * 100 - 100
@@ -151,11 +156,6 @@ for (n in 1:length(run_ids)) {
   av_rest <- mean(all_res[all_res$run_id == id & all_res$state == "REST",]$tCho)
   tCho_perc <- all_res[all_res$run_id == id,]$tCho / av_rest * 100 - 100
   all_res[all_res$run_id == id,]$tCho_perc <- tCho_perc
-  
-  # Asc
-  av_rest <- mean(all_res[all_res$run_id == id & all_res$state == "REST",]$Asc)
-  Asc_perc <- all_res[all_res$run_id == id,]$Asc / av_rest * 100 - 100
-  all_res[all_res$run_id == id,]$Asc_perc <- Asc_perc
   
   # Gln
   av_rest <- mean(all_res[all_res$run_id == id & all_res$state == "REST",]$Gln)
@@ -194,14 +194,23 @@ for (n in 1:length(run_ids)) {
                                  sd(all_res[task_set,]$Glu_perc), 2)
 }
 
-metab_cols <- c("Glu_perc", "Lac_perc", "Asp_perc", "Glc_perc", "GABA_perc",
-                "tCr_perc", "tCho_perc", "tNAA_perc", "Asc_perc", "Gln_perc",
-                "GSH_perc", "Ins_perc", "PEth_perc", "sIns_perc", "Tau_perc")
+metab_cols <- c("Glu_perc", "Lac_perc",  "Asp_perc",  "Glc_perc",  "GABA_perc",
+                "tCr_perc", "tCho_perc", "tNAA_perc", "Asc_perc",  "Gln_perc",
+                "GSH_perc", "Ins_perc",  "PEth_perc", "sIns_perc", "Tau_perc")
 
-all_res_perc_change_abf <- all_res |> 
-                       filter(state == "TASK", fit_method == "abfit-reg", 
-                              n_blocks == "5 blocks") |>
-                       pivot_longer(cols = all_of(metab_cols))
+sel_cols <- c("field_strength", "basis_type", metab_cols)
+
+# all_res_perc_change_abf <- all_res |>
+#                        filter(state == "TASK", fit_method == "abfit-reg",
+#                               n_blocks == "5 blocks") |>
+#                               pivot_longer(cols = all_of(metab_cols))
+
+all_res_perc_change_abf <- all_res |>
+  filter(state == "TASK", fit_method == "abfit-reg",
+         n_blocks == "5 blocks") |>
+  select(all_of(sel_cols)) |>
+  pivot_longer(cols = all_of(metab_cols))
+                               
 
 all_res_perc_change_abf$name <- gsub('.{5}$', '', all_res_perc_change_abf$name)
 
@@ -220,6 +229,7 @@ sd_p <- all_res_perc_change_abf |> ggplot(aes(x = name, y = value)) +
   ylab("s.d. bias (%)") + xlab("") + theme(legend.title=element_blank())
 
 cohens_d <- function(x) mean(x) / sd(x)
+
 # cd
 cd_p <- all_res_perc_change_abf |> ggplot(aes(x = name, y = value)) + 
   stat_summary(fun = cohens_d, aes(col = basis_type), geom = "point") + 
@@ -230,9 +240,19 @@ cd_p <- all_res_perc_change_abf |> ggplot(aes(x = name, y = value)) +
 plot_grid(mean_p, sd_p, cd_p, nrow = 3, labels = c("A", "B", "C"), align = "v")
 ggsave("abfit_metab_bias_cd.png", width = 8, height = 8)
 
-plot_grid(mean_p, sd_p, nrow = 2, labels = c("A", "B"), align = "v")
-ggsave("abfit_metab_bias.png", width = 8, height = 6)
+# plot without Cohen's d
+# plot_grid(mean_p, sd_p, nrow = 2, labels = c("A", "B"), align = "v")
+# ggsave("abfit_metab_bias.png", width = 8, height = 6)
 
+# all_res_perc_change_abf |>
+#   filter(field_strength == "3T", basis_type == "default\nanalysis") |> 
+#   ggplot(aes(x = name, y = value)) + 
+#   geom_hline(yintercept = 0) +
+#   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+#   ylab("Mean bias (%)") + xlab("") + theme(legend.title=element_blank()) +
+#   geom_jitter(alpha = 0.2) +
+#   stat_summary(fun = mean, aes(col = basis_type), geom = "point") + 
+#   ylim(-10, 10)
 
 all_res_perc_change_lcm <- all_res |> 
                        filter(state == "TASK", fit_method == "lcmodel", 
@@ -266,5 +286,6 @@ cd_p <- all_res_perc_change_lcm |> ggplot(aes(x = name, y = value)) +
 plot_grid(mean_p, sd_p, cd_p, nrow = 3, labels = c("A", "B", "C"), align = "v")
 ggsave("lcmodel_metab_bias_cd.png", width = 8, height = 8)
 
-plot_grid(mean_p, sd_p, nrow = 2, labels = c("A", "B"), align = "v")
-ggsave("lcmodel_metab_bias.png", width = 8, height = 6)
+# plot without Cohen's d
+# plot_grid(mean_p, sd_p, nrow = 2, labels = c("A", "B"), align = "v")
+# ggsave("lcmodel_metab_bias.png", width = 8, height = 6)
